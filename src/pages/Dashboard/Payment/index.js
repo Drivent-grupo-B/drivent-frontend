6cd8dc5dccfdc8e2f1e2f8d21dacb0dcc7b46b2d
Filipe Garrote <filipe.garrote@gmail.com>
2022-12-16 01:00:16 -0300

    feat: added event type section on payment page (just layout)

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,62 @@
+import { Typography } from '@material-ui/core';
+import styled from 'styled-components';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  return (
+    <>
+      <StyledTypography variant="h4">Ingresso e pagamento</StyledTypography>
+      <EventTypeContainer>
+        <h2>Primeiro, escolha sua modalidade de ingresso</h2>
+        <div>
+          <PaymentOption>
+            <h3>Presencial</h3>
+            <span>R$ 250</span>
+          </PaymentOption>
+          <PaymentOption>
+            <h3>Online</h3>
+            <span>R$ 100</span>
+          </PaymentOption>
+        </div>
+      </EventTypeContainer>
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 27px !important;
+`;
+
+const EventTypeContainer = styled.section`
+  h2 {
+    font-size: 20px;
+    color: #8e8e8e;
+  }
+
+  > div {
+    display: flex;
+    gap: 24px;
+    margin-top: 17px;
+  }
+`;
+
+const PaymentOption = styled.div`
+  width: 145px;
+  height: 145px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+  border: 1px solid #cecece;
+  border-radius: 20px;
+
+  h3 {
+    font-size: 16px;
+    color: #454545;
+  }
+
+  span {
+    font-size: 14px;
+    color: #898989;
+  }
+`;
