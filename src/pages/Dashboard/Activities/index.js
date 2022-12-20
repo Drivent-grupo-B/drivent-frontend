631fe333246b8a98f102e9dcb0bde66577b328fe
EduardoVSR1995 <eduardvitor7@gmail.com>
2022-12-20 16:32:03 -0300

    feat: choice of description for online modality

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -2,21 +2,38 @@ import { Typography } from '@material-ui/core';
 import styled from 'styled-components';
 import useTicket from '../../../hooks/api/useTicket.js';
 
-export default function Activities() {
-  const { ticket } = useTicket();
-
+function DescriptionChoice(ticket) {
+  if( !ticket.ticketTypeId.isRemote ) {
+    return(
+      <>
+        Você não precisa escolher as atividades
+      </>
+    );
+  }
   return(
     <>
-      <StyledTypography variant="h4">Escolha de atividades</StyledTypography>        
+      Você precisa ter confirmado pagamento antes 
+      <br/>
+      de fazer a escolha de atividade
+    </>
+  );
+}
+
+export default function Activities() {
+  const { ticket } = useTicket();  
+
+  return (
+    <>
+      <StyledTypography variant="h4">Escolha de atividades</StyledTypography>
       <ActivitContainer>
-        {!ticket || ticket?.status === 'RESERVED' ?
+        { ticket && ticket.status === 'RESERVED' ?
           <CenterContainer>
-            Você precisa ter confirmado pagamento antes <br/>de fazer a escolha de atividade 
+            {DescriptionChoice(ticket)}
           </CenterContainer>
           :
           'ola'
         }
-      </ActivitContainer>     
+      </ActivitContainer>
     </>
   );
 }
