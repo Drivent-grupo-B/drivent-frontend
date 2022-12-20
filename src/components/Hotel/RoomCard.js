6d22267a4df10fd6ca65d9493350b5137baf76b9
Filipe Garrote <filipe.garrote@gmail.com>
2022-12-20 03:16:36 -0300

    feat: added disabling full rooms, preventing user from selecting it

--- src/components/Hotel/RoomCard.js
+++ src/components/Hotel/RoomCard.js
@@ -1,15 +1,19 @@
 import styled from 'styled-components';
 import { IoPersonOutline, IoPerson } from 'react-icons/io5';
 import useRoomBooking from '../../hooks/api/useRoomBooking';
+import { useEffect, useState } from 'react';
 
 export default function RoomCard({ room }) {
   const { roomBooking } = useRoomBooking(room.id);
-  if (roomBooking === null) return '';
+  const [isFull, setIsFull] = useState(false);
+  const [iconsList, setIconsList] = useState([]);
 
-  function verifyRoomCapacity() {
+  useEffect(() => {
+    if (roomBooking === null) return '';
     const vacancies = {};
     let totalCapacity = room.capacity;
     let totalOfBookings = roomBooking.length;
+    if (totalCapacity === totalOfBookings) setIsFull(true);
 
     while (totalCapacity > 0) {
       vacancies[totalCapacity] = true;
@@ -18,13 +22,13 @@ export default function RoomCard({ room }) {
       totalOfBookings -= 1;
     }
 
-    return Object.values(vacancies);
-  }
+    setIconsList(Object.values(vacancies));
+  }, [roomBooking, room]);
 
   return (
-    <CardStyle>
+    <CardStyle isFull={isFull}>
       <h5>{room.name}</h5>
-      <div>{verifyRoomCapacity().map((vacancy) => (vacancy ? <IoPersonOutline /> : <IoPerson />))}</div>
+      <div>{iconsList.map((vacancy) => (vacancy ? <IoPersonOutline /> : <IoPerson />))}</div>
     </CardStyle>
   );
 }
@@ -34,18 +38,20 @@ const CardStyle = styled.div`
   height: 45px;
   border-radius: 10px;
   border: 1px #cecece solid;
+  background-color: ${(props) => (props.isFull ? '#E9E9E9' : '#ffffff')};
   padding: 11px 16px;
   display: flex;
   align-items: center;
   justify-content: space-between;
 
   h5 {
+    color: ${(props) => (props.isFull ? '#9D9D9D' : '#454545')};
     font-size: 20px;
     font-weight: 700;
   }
 
   svg {
-    color: #000000;
+    color: ${(props) => (props.isFull ? '#8C8C8C' : '#000000')};
     margin-top: 5px;
   }
 `;
