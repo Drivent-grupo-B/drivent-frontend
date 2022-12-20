157bb21ea93c97c9a342138f3e6cb504151e23f3
EduardoVSR1995 <eduardvitor7@gmail.com>
2022-12-20 15:55:02 -0300

    feat: splash screen without ticket payment

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,37 @@
+import { Typography } from '@material-ui/core';
+import styled from 'styled-components';
+import useTicket from '../../../hooks/api/useTicket.js';
+
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  const { ticket } = useTicket();
+
+  return(
+    <>
+      <StyledTypography variant="h4">Escolha de atividades</StyledTypography>        
+      <ActivitContainer>
+        {!ticket || ticket?.status === 'RESERVED' ?
+          <CenterContainer>
+            Você precisa ter confirmado pagamento antes <br/>de fazer a escolha de atividade 
+          </CenterContainer>
+          :
+          'ola'
+        }
+      </ActivitContainer>     
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 27px !important;
+`;
+
+const CenterContainer = styled.div`
+  margin-top: 25% ;
+  text-align: center ;
+`;
+
+const ActivitContainer = styled.div`
+  font-size: 20px;
+  line-height: 23px;
+  color: #8e8e8e;
+`;
--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -6,31 +6,36 @@ import Hotels from './Hotels.js';
 export default function Hotel() {
   const { ticket } = useTicket();
 
-  return(
+  return (
     <>
-      <StyledTypography variant="h4">Escolha de hotel e quarto</StyledTypography>        
+      <StyledTypography variant="h4">Escolha de hotel e quarto</StyledTypography>
       <HotelContainer>
         {
           !ticket || ticket?.status === 'RESERVED' ?
-            <h2>
-             Você precisa ter confirmado pagamento antes de fazer a escolha de hospedagem
-            </h2>
+            <CenterContainer>
+              Você precisa ter confirmado pagamento antes <br />de fazer a escolha de hospedagem
+            </CenterContainer>
             :
             (ticket?.TicketType.isRemote || !ticket?.TicketType.includesHotel) ?
               <h2>
-            Sua modalidade de ingresso não inclui hospedagem 
-            Prossiga para a escolha de atividades
+                Sua modalidade de ingresso não inclui hospedagem
+                Prossiga para a escolha de atividades
               </h2>
               :
               <Hotels />
         }
-      </HotelContainer>     
+      </HotelContainer>
     </>
   );
 }
 
 const StyledTypography = styled(Typography)`
   margin-bottom: 27px !important;
+`;
+
+const CenterContainer = styled.div`
+  margin-top: 25% ;
+  text-align: center ;
 `;
 
 const HotelContainer = styled.div`
