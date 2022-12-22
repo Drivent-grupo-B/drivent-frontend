657728691793c92dc39b4eff6de04088e8afc524
gabao55 <gabriel.s.rosin@gmail.com>
2022-12-22 09:20:16 -0300

    fix: now user can switch between day activities with no refresh

--- src/pages/Dashboard/Activities/ScheduleEvent.js
+++ src/pages/Dashboard/Activities/ScheduleEvent.js
@@ -1,14 +1,11 @@
-import { Typography } from '@material-ui/core';
-import { useState } from 'react';
 import styled from 'styled-components';
 import useDaySchedule from '../../../hooks/api/useDaySchedule.js';
 
 export default function ScheduleEvent({ schedule }) {
   const { scheduleDay } = useDaySchedule(schedule.schedule.dayId);
-  console.log(scheduleDay);
   return(
     <ScheduleContainer>
-        sdsadasa
+      {`dia ${schedule.schedule.dayId}`}
     </ScheduleContainer>
   );
 }
